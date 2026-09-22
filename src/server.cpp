19d8bc9f96a576366642ebf2e6fbb9d5b15cccdb
srikala.sindhu@gmail.com <srimathianbalagan28@gmail.com>
2026-09-22 22:24:18 +0530

    Connect C++ backend with SQLite

--- src/server.cpp
+++ src/server.cpp
@@ -1,5 +1,7 @@
 #include <winsock2.h>
 #include <iostream>
+#include <string>
+#include <sqlite3.h>
 
 #pragma comment(lib, "ws2_32.lib")
 
@@ -7,25 +9,67 @@ using namespace std;
 
 int main()
 {
+    sqlite3 *db;
+
+    if (sqlite3_open("akshayamart.db", &db) != SQLITE_OK)
+    {
+        cout << "Database connection failed!" << endl;
+        return 1;
+    }
+
+    cout << "Database connected successfully!" << endl;
+
     WSADATA wsa;
-    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+    {
+        cout << "Winsock startup failed!" << endl;
+        sqlite3_close(db);
+        return 1;
+    }
 
     SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (serverSocket == INVALID_SOCKET)
+    {
+        cout << "Socket creation failed!" << endl;
+        WSACleanup();
+        sqlite3_close(db);
+        return 1;
+    }
 
     sockaddr_in serverAddress{};
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_addr.s_addr = INADDR_ANY;
     serverAddress.sin_port = htons(8080);
 
-    bind(serverSocket, (sockaddr*)&serverAddress, sizeof(serverAddress));
-    listen(serverSocket, 5);
+    if (bind(serverSocket, (sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR)
+    {
+        cout << "Bind failed!" << endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        sqlite3_close(db);
+        return 1;
+    }
 
-    cout << "AkshayaMart server started!" << endl;
-    cout << "Open: http://localhost:8080" << endl;
+    if (listen(serverSocket, 5) == SOCKET_ERROR)
+    {
+        cout << "Listen failed!" << endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        sqlite3_close(db);
+        return 1;
+    }
+
+    cout << "AkshayaMart backend server started!" << endl;
+    cout << "Open http://localhost:8080" << endl;
 
     while (true)
     {
         SOCKET clientSocket = accept(serverSocket, nullptr, nullptr);
+
+        if (clientSocket == INVALID_SOCKET)
+            continue;
 
         string response =
             "HTTP/1.1 200 OK\r\n"
@@ -35,8 +79,8 @@ int main()
             "<html>"
             "<head><title>AkshayaMart</title></head>"
             "<body>"
-            "<h1>Welcome to AkshayaMart</h1>"
-            "<p>C++ Backend Server is Running</p>"
+            "<h1>AkshayaMart Backend</h1>"
+            "<p>C++ Backend and SQLite Database are connected!</p>"
             "</body>"
             "</html>";
 
@@ -47,6 +91,7 @@ int main()
 
     closesocket(serverSocket);
     WSACleanup();
+    sqlite3_close(db);
 
     return 0;
 }
